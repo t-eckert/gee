1fc18b6ad29eff06da5bb9756d627bd9a5226f8c
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-12-23 20:13:18 -0500

    Oops! I left a temporary thing in...

--- gee/src/downstream/file.rs
+++ gee/src/downstream/file.rs
@@ -2,8 +2,6 @@ use std::fs;
 
 // TODO: Have this return a standard error. Same result as call_application.
 pub fn serve_file(path: &str) -> Option<Vec<u8>> {
-    let path = "../test/test_application/static/sample.json";
-
     let contents = fs::read(path).expect("could not read file");
 
     Some(contents)
