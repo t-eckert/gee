cd1b63588815ef4ecc29dd42f4c80933396b41d8
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-12-23 20:12:20 -0500

    It can serve static files!

--- gee/src/downstream/file.rs
+++ gee/src/downstream/file.rs
@@ -1,3 +1,10 @@
+use std::fs;
+
+// TODO: Have this return a standard error. Same result as call_application.
 pub fn serve_file(path: &str) -> Option<Vec<u8>> {
-    Some("Some file".as_bytes().to_owned())
+    let path = "../test/test_application/static/sample.json";
+
+    let contents = fs::read(path).expect("could not read file");
+
+    Some(contents)
 }
