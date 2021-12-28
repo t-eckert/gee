019bb9e4d2d96a33f964966026cce0bfae16c97e
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-12-28 17:28:21 -0500

    Clean up imports on service mod

--- gee/src/upstream/service.rs
+++ gee/src/upstream/service.rs
@@ -1,3 +1,8 @@
+use crate::{
+    downstream::{call_application, serve_file},
+    environ::Environ,
+};
+use hyper::{service::Service as HyperService, Body, Request, Response};
 use log::{debug, info};
 use std::{
     collections::HashMap,
@@ -5,14 +10,7 @@ use std::{
     task::{Context, Poll},
 };
 
-use hyper::{service::Service as HyperService, Body, Request, Response};
-
-use crate::{
-    downstream::{call_application, serve_file},
-    environ::Environ,
-};
-
-/// Service handles the requests received by Gee, routing them to Python or serving static files back to the caller.
+/// `Service` handles the requests received by Gee, routing them to Python or serving static files back to the caller.
 pub struct Service {
     /// `root_dir` is the absolute path to the directory where Gee is running.
     pub root_dir: String,
@@ -31,6 +29,12 @@ impl Service {
             .any(|(server_path, _)| path.starts_with(server_path))
     }
 
+    /// `resolve_static_path` receives the `path` from the URI (e.g. /static/hello.txt) and checks it against the
+    /// `static_routes` defined on the service. These `static_routes` map URI paths to UNIX-like paths (e.g.
+    /// /static => ./static/). If there exists a key in `static_routes` which begins with the same characters
+    /// as the `path`, the key will be stripped from the beginning of the `path` and replaced with corresponding
+    /// value so that the server can look up the file and serve it to the user. If the resulting `path` is a directory,
+    /// `index.html` will be appended to the path so that the default web page may be served.
     fn resolve_static_path(&self, path: &str) -> Option<String> {
         let matching_route = self
             .static_routes
@@ -63,22 +67,24 @@ impl HyperService<Request<Body>> for Service {
         Ok(()).into()
     }
 
+    /// `call` receives a request from the user and routes it to the `serve_file` function if the request is for a
+    /// static asset, otherwise an `Environ` object is built from the request and passed to the `call_application`
+    /// function which will execute the request against the Python web application according to the WSGI spec.
     fn call(&mut self, req: Request<Body>) -> Self::Future {
-        let rsp = Response::builder();
-
         info!("{} request received at {}", req.method(), req.uri());
         debug!("{:#?}", req);
 
-        let environ = Environ::from_request(&req);
         let request_result = if self.is_static_request(req.uri().path()) {
             let static_path = self
                 .resolve_static_path(req.uri().path())
                 .expect("Cannot resolve static path");
             serve_file(&static_path)
         } else {
+            let environ = Environ::from_request(&req);
             call_application(environ)
         };
 
+        let rsp = Response::builder();
         let response = match request_result {
             Some(content) => rsp.status(200).body(Body::from(content)).unwrap(),
             None => rsp.status(404).body(Body::from(vec![])).unwrap(),
