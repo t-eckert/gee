12142841baf836219ed34cc9075cd7e6e6281471
Thomas Eckert <thomas.james.eckert@gmail.com>
2022-03-18 17:32:12 -0400

    Remove "downstream" vs "upstream" model

--- gee/src/downstream/mod.rs
+++
@@ -1,5 +0,0 @@
-mod application;
-mod file;
-
-pub use application::call_application;
-pub use file::serve_file;
--- gee/src/upstream/mod.rs
+++
@@ -1,5 +0,0 @@
-mod server;
-mod service;
-mod service_builder;
-
-pub use self::server::Server;
